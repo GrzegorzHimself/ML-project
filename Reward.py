f5dc973a1e3255eb32a6ddb2fc96c3a9aa90ba37
GrzegorzHimself <79000379+GrzegorzHimself@users.noreply.github.com>
2024-12-16 18:10:43 +0100

    Update Reward.py
    Consistency with names

--- Reward.py
+++ Reward.py
@@ -1,18 +1,18 @@
 class Reward:
     @staticmethod
-    def reward_chaser_calculation(position_victim, position_chaser):
-        x, y = position_victim
-        x1, y1 = position_chaser
+    def reward_hunter_calculation(position_pray, position_hunter):
+        x, y = position_pray
+        x1, y1 = position_hunter
         if x == x1 and y == y1:
             return 10
-        distance = (x-x1)**2 + (y-y1)**2
-        return 1/distance
+        distance = (x - x1) ** 2 + (y - y1) ** 2
+        return 1 / distance
 
     @staticmethod
-    def reward_victim_calculation(position_victim, position_chaser):
-        x, y = position_victim
-        x1, y1 = position_chaser
+    def reward_pray_calculation(position_pray, position_hunter):
+        x, y = position_pray
+        x1, y1 = position_hunter
         if x == x1 and y == y1:
             return -10
-        distance = (x-x1)**2 + (y-y1)**2
-        return distance*0.1
+        distance = (x - x1) ** 2 + (y - y1) ** 2
+        return distance * 0.1
